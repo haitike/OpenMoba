6963ae2b78175f3e643a555b9fe18d3b00994aeb
haitike <haitike@uruk.(none)>
2011-01-24 10:57:01 +0100

    new files

--- client/main.cpp
+++ client/main.cpp
@@ -1,101 +1,33 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2011)
 and may not be redestributed without written permission.*/
 
-//The headers
+// Project headers
+#include "tools.h"
+
+// External headers
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
 #include <string>
 
 //Screen attributes
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 const int SCREEN_BPP = 32;
 
 //The surfaces
 SDL_Surface *image = NULL;
 SDL_Surface *screen = NULL;
 
-SDL_Surface *load_image( std::string filename )
-{
-    //The image that's loaded
-    SDL_Surface* loadedImage = NULL;
-
-    //The optimized image that will be used
-    SDL_Surface* optimizedImage = NULL;
-
-    //Load the image using SDL_image
-    loadedImage = IMG_Load( filename.c_str() );
-
-    //If the image loaded
-    if( loadedImage != NULL )
-    {
-        //Create an optimized image
-        optimizedImage = SDL_DisplayFormat( loadedImage );
-
-        //Free the old image
-        SDL_FreeSurface( loadedImage );
-    }
-
-    //Return the optimized image
-    return optimizedImage;
-}
-
-void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination )
-{
-    //Rectangle to hold the offsets
-    SDL_Rect offset;
-
-    //Get offsets
-    offset.x = x;
-    offset.y = y;
-
-    //Blit the surface
-    SDL_BlitSurface( source, NULL, destination, &offset );
-}
-
-bool init()
-{
-    //Initialize all SDL subsystems
-    if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
-    {
-        return false;
-    }
-
-    //Set up the screen
-    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
-
-    //If there was an error in setting up the screen
-    if( screen == NULL )
-    {
-        return false;
-    }
-
-    //Set the window caption
-    SDL_WM_SetCaption( "PNG test", NULL );
-
-    //If everything initialized fine
-    return true;
-}
-
-void clean_up()
-{
-    //Free the surface
-    SDL_FreeSurface( image );
-
-    //Quit SDL
-    SDL_Quit();
-}
-
 int main( int argc, char* args[] )
 {
-    //Initialize
-    if( init() == false )
+    //Initialize. Caption, Screen Image, Width, height and BPP.
+    if( init("OpenMoba", screen, SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP ) == false )
     {
         return 1;
     }
 
     //Load the image
-    image = load_image( "img/tower.png" );
+    image = load_image( "img/nexus.png" );
 
     //If there was a problem in loading the image
     if( image == NULL )
---
+++ client/tools.cpp
@@ -0,0 +1,69 @@
+#include "tools.h"
+
+SDL_Surface *load_image( std::string filename )
+{
+    //The image that's loaded
+    SDL_Surface* loadedImage = NULL;
+
+    //The optimized image that will be used
+    SDL_Surface* optimizedImage = NULL;
+
+    //Load the image using SDL_image
+    loadedImage = IMG_Load( filename.c_str() );
+
+    //If the image loaded
+    if( loadedImage != NULL )
+    {
+        //Create an optimized image
+        optimizedImage = SDL_DisplayFormat( loadedImage );
+
+        //Free the old image
+        SDL_FreeSurface( loadedImage );
+    }
+
+    //Return the optimized image
+    return optimizedImage;
+}
+
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination )
+{
+    //Rectangle to hold the offsets
+    SDL_Rect offset;
+
+    //Get offsets
+    offset.x = x;
+    offset.y = y;
+
+    //Blit the surface
+    SDL_BlitSurface( source, NULL, destination, &offset );
+}
+
+bool init(std::string caption, SDL_Surface* screen, int screen_width, int screen_height, int screen_bpp)
+{
+    //Initialize all SDL subsystems
+    if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        return false;
+    }
+
+    //Set up the screen
+    screen = SDL_SetVideoMode( screen_width, screen_height, screen_bpp, SDL_SWSURFACE );
+
+    //If there was an error in setting up the screen
+    if( screen == NULL )
+    {
+        return false;
+    }
+
+    //Set the window caption
+    SDL_WM_SetCaption( caption, NULL );
+
+    //If everything initialized fine
+    return true;
+}
+
+void clean_up()
+{
+    SDL_FreeSurface( image );
+    SDL_Quit();
+}
